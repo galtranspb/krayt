c12f447ee4df4feedf51596eb97f8fa3a927b681
AnatolySokolov <anatoliysokolov1981@gmail.com>
2020-02-12 13:47:46 +0300

    refactoring

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,16 +1,7 @@
-import './logo.svg';
-import './check-circle.svg';
 import './banner-bg.png';
-import './cog.svg';
-import './file-alt.svg';
-import './fire-extinguisher.svg';
-import './life-ring.svg';
-import './paint-brush.svg';
-import './tags.svg';
 import './reasons-bg.png';
 import './screenshots-bg.png';
 import './screenshots-first.jpg';
-import './arrow.svg';
 import './prices-bg.png';
 import './video-bg.jpg';
 import './first.jpg';
@@ -19,8 +10,3 @@ import './third.jpg';
 import './mac_icon.png';
 import './android_icon.png';
 import './windows_icon.png';
-
-import './behance.svg';
-import './dribbble.svg';
-import './facebook.svg';
-import './twitter.svg';
